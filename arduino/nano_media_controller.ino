b1d107b80c1476ff8689574d9119187ae1d33094
202420232022 <jntk6174@gmail.com>
2026-01-12 21:18:47 +0900

    Update nano_media_controller.ino

--- arduino/nano_media_controller.ino
+++ arduino/nano_media_controller.ino
@@ -1,10 +1,28 @@
 // Arduino Nano main sketch
 
+const int buttonPin = 2;
+bool lastState = HIGH;
+unsigned long lastTime = 0;
+const unsigned long debounceMs = 50;
+
 void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
+  Serial.println("Arduino ready");
 }
 
 void loop() {
-  Serial.println("Hello");
-  delay(1000);
+  // put your main code here, to run repeatedly:
+  bool currentState = digitalRead(buttonPin);
+  unsigned long now = millis();
+
+  if (lastState == HIGH && currentState == LOW) {
+    if (now - lastTime > debounceMs) {
+      Serial.println("PLAY");
+      lastTime = now;
+    }
+  }
+  
+  lastState = currentState;
 }
