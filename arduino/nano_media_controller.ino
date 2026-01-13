2049fad4e42f0e8d7a06159c4e40736de5af9b13
202420232022 <jinogame1210@gmail.com>
2026-01-13 22:55:54 +0900

    add the new Pin

--- arduino/nano_media_controller.ino
+++ arduino/nano_media_controller.ino
@@ -1,28 +1,34 @@
 // Arduino Nano main sketch
 
-const int buttonPin = 2;
-bool lastState = HIGH;
+const int PlayPin = 2; // Pin for Play/Pause button
+const int VolUPin = 3; // Pin for Volume Up button
+const int VolDPin = 4; // Pin for Volume Down button
+const int NextPin = 5; // Pin for Next Track button
+bool PlayState = HIGH;
 unsigned long lastTime = 0;
-const unsigned long debounceMs = 50;
+const unsigned long debounceMs = 100;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(PlayPin, INPUT_PULLUP);
+  pinMode(VolUPin, INPUT_PULLUP);
+  pinMode(VolDPin, INPUT_PULLUP);
+  pinMode(NextPin, INPUT_PULLUP);
   Serial.begin(9600);
   Serial.println("Arduino ready");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bool currentState = digitalRead(buttonPin);
+  bool currentState = digitalRead(PlayPin);
   unsigned long now = millis();
 
-  if (lastState == HIGH && currentState == LOW) {
+  if (PlayState == HIGH && currentState == LOW) {
     if (now - lastTime > debounceMs) {
       Serial.println("PLAY");
       lastTime = now;
     }
   }
   
-  lastState = currentState;
+  PlayState = currentState;
 }
