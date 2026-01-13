980cce37861e93df6becb723e2498c08945cc1ea
202420232022 <jinogame1210@gmail.com>
2026-01-13 23:47:09 +0900

    Add New Command

--- arduino/nano_media_controller.ino
+++ arduino/nano_media_controller.ino
@@ -5,6 +5,9 @@ const int VolUPin = 3; // Pin for Volume Up button
 const int VolDPin = 4; // Pin for Volume Down button
 const int NextPin = 5; // Pin for Next Track button
 bool PlayState = HIGH;
+bool VolUState = HIGH;
+bool VolDState = HIGH;
+bool NextState = HIGH;
 unsigned long lastTime = 0;
 const unsigned long debounceMs = 100;
 
@@ -20,15 +23,45 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bool currentState = digitalRead(PlayPin);
+  bool NPlayState = digitalRead(PlayPin);
+  bool NVolUState = digitalRead(VolUPin);
+  bool NVolDState = digitalRead(VolDPin);
+  bool NNextState = digitalRead(NextPin);
   unsigned long now = millis();
 
-  if (PlayState == HIGH && currentState == LOW) {
+  if (PlayState == HIGH && NPlayState == LOW) {
     if (now - lastTime > debounceMs) {
       Serial.println("PLAY");
       lastTime = now;
     }
   }
   
-  PlayState = currentState;
+  PlayState = NPlayState;
+
+  if (VolUState == HIGH && NVolUState == LOW) {
+    if (now - lastTime > debounceMs) {
+      Serial.println("VOL_UP");
+      lastTime = now;
+    }
+  }
+
+  VolUState = NVolUState;
+
+  if (VolDState == HIGH && NVolDState == LOW) {
+    if (now - lastTime > debounceMs) {
+      Serial.println("VOL_DOWN");
+      lastTime = now;
+    }
+  }
+
+  VolDState = NVolDState;
+
+  if (NextState == HIGH && NNextState == LOW) {
+    if (now - lastTime > debounceMs) {
+      Serial.println("NEXT");
+      lastTime = now;
+    }
+  }
+
+  NextState = NNextState;
 }
